9ec5ac9413f4144c01bee9501f4c6813c56c05df
Alice Chen <alicechen118@gmail.com>
2017-09-10 22:39:04 -0700

    Add user input for wanted password length

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?#!$%@^&*+-_=;:\'",./<>'
+length = int(input('Password length? '))
 
-password = random.choice(chars)
+password = ''
+for i in range(length):
+    password += random.choice(chars)
 print(password)
