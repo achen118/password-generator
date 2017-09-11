43770b21548cb5a1ee88f902eb9285719f968a08
Alice Chen <alicechen118@gmail.com>
2017-09-10 22:42:20 -0700

    Add option to generate more than one password at a time

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?#!$%@^&*+-_=;:\'",./<>'
+num_of_passwords = int(input('How many passwords to generate? '))
 length = int(input('Password length? '))
 
-password = ''
-for i in range(length):
-    password += random.choice(chars)
-print(password)
+for i in range(num_of_passwords):
+    password = ''
+    for j in range(length):
+        password += random.choice(chars)
+    print(password)
